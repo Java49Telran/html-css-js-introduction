cd1c035355984942f4b59aca658f67477cb0b381
Yuragranovsky <yuragranovsky@gmail.com>
2022-10-19 21:05:52 +0300

    CW #9 add / remove classes , class hidden

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
+
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on ", anchors[i]);
+        showDetails();
         setDetails(anchors[i]);
     })
+}
+function showDetails() {
+    mainClass.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
