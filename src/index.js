2e4075adc509ecbe3ef0378ca13101e2ab5f3160
Yuragranovsky <yuragranovsky@gmail.com>
2022-10-23 21:11:10 +0300

    CW #10

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
@@ -18,6 +20,11 @@ for(let i = 0; i < anchors.length; i++) {
 }
 function showDetails() {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+    },1)
+
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
