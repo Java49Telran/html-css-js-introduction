78fb08b9e4f5ad798564d4a8c381f5835bc11574
Yuragranovsky <yuragranovsky@gmail.com>
2022-10-26 21:01:05 +0300

    cw #11 - first getting started of JS as a programming language

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// console.log("Hello world");
+// for( i = 0; i < 3; i++) {
+//     setTimeout(function(){
+//         console.log(i)
+//     });
+// }
+ i = 100;
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(function () { console.log(i) });
+// } => 0 1 2
+
+// for (var i = 0; i < 3; i++) {
+//      console.log(i) ;
+// }
+// function sum(op1, op2) {
+//     let res = op1 + op2;
+//     return res;
+// }
+// let op1 = 10;
+// let op2 = 20;
+// let res = sum(op1, op2);
+// console.log(res)
+function sumDigits(number) {
+    //TODO
+    //function should return sum of a given number digits
+}
+//Example
+console.log(sumDigits(123)); //should be printed out 6
+
