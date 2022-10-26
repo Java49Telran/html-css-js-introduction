c67471d6dbfa4b8a3ee9a4d6580c4d0da84c08d2
Yuragranovsky <yuragranovsky@gmail.com>
2022-10-26 15:10:45 +0300

    HW #10 solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
 const detailsContainer = document.querySelector(".details-container");
+const audio = document.getElementById("audio")
 const HIDDEN = "hidden";
 const IS_POINT = "is-point";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+    audio.setAttribute("src", anchor.getAttribute('data-sound'))
+    audio.play();
+    setTimeout(function () {
+        audio.pause();
+        audio.currentTime = 0;
+    }, 5000)
 }
 
 for(let i = 0; i < anchors.length; i++) {
