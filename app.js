9e59c8e7bbfbf496cd5f2673fbc21f94476da636
Yuragranovsky <yuragranovsky@gmail.com>
2022-11-02 21:30:14 +0200

    CW #13

--- app.js
+++ app.js
@@ -1,146 +1,44 @@
-
-// console.log("Hello world");
-// for( i = 0; i < 3; i++) {
-//     setTimeout(function(){
-//         console.log(i)
-//     });
+let ar = [];
+ar[10000] = 100;
+ar[1] = [1, 2, 3];
+console.log("length of array = ",ar.length);
+ar[0] = "hello";
+console.log("10000-th element = ", ar[10000]);
+console.log("0-th element = ", ar[0]);
+console.log("1-th element = ", ar[1]);
+let str = "Hello";
+let arStr = Array.from(str); //getting array of the string's symbols
+console.log("String 'Hello' -> array is ", arStr);
+// for (let i = 0; i < arStr.length; i++) {
+//     console.log("element at index ", i, arStr[i]);
 // }
- i = 100;
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(function () { console.log(i) });
-// } => 0 1 2
-
-// for (var i = 0; i < 3; i++) {
-//      console.log(i) ;
-// }
-// function sum(op1, op2) {
-//     let res = op1 + op2;
-//     return res;
-// }
-// let op1 = 10;
-// let op2 = 20;
-// let res = sum(op1, op2);
-// console.log(res)
-/************************************************** */
-//HW #11
-// function sumDigits(number) {
-//     //compute sum of digits for the integer numbers
-//    number = Math.abs(number);
-//    let sum = 0;
-//    number = Math.trunc(number); //get rid of the fractional part
-//    do {
-//         let digit = number % 10;
-//         number = (number - digit) / 10;
-//         sum += digit;
-//    }while(number != 0);
-//   return sum;
-
-// }
-// //Example
-// let sum = sumDigits(123)
-// console.log(sum); //should be printed out 6
-
-
-/*********************************CW 12 */
-// let strNum1 = "123";
-// let strNum2 = '9';
-// console.log(strNum1 + strNum2); //=> 1239
-// console.log(strNum1 - strNum2); //=> 114
-// console.log(strNum1 > strNum2);
-//conversions from string to number
-// let num1 = +strNum1;
-// let num2 = +strNum2;
-// console.log(num1 + num2); //=> 132
-// console.log(num1 - num2); //=> 114
-// console.log(num1 > num2); //true
-// let strNumStr = '12.3ab';
-// let numStr = +strNumStr;
-// console.log(numStr);
-// let num = parseInt(strNumStr);
-// if (isNaN(numStr)) {
-//     console.log("numStr is NaN")
-// }
-// let num10 = 123;
-// let strNum10 = "" + num10;
-// let strNum16 = num10.toString(36);
-// console.log(strNum10, strNum16);
-
-//conversion from Morse code to number
-//symbol "." -> 0 ; "-" -> 1
-// function  fromMorseToNumber(morseCode) {
-//     let result = 0;
-//     for (let i = 0; i < morseCode.length; i++) {
-//         let code = morseCode[i] == '.' ? 0 : 1;
-//         result = result * 2 + code;
-//     }
-//     return result;
-// }
-// console.log(fromMorseToNumber('-.-...-.---.-.-..--'));
-// function fromNumberToMorse(number) {
-//     number = Math.abs(number);
-//     let res = "";
-//     do {
-//         let digit = number % 2;
-//         let sym = digit == 0 ? '.' : '-';
-//         res = sym + res;
-//         number = Math.trunc(number / 2);
-
-//     } while(number != 0);
-//     return res;
-// }
-// console.log(fromNumberToMorse(333651));
-function getSymbol(digit) {
-    let codeA = 'a'.charCodeAt();
-    if (digit > 9) {
-        digit = String.fromCharCode(codeA + digit - 10);
-    }
-      
-   return digit;
-    //31 - 10 + 97
+function println(element, index, array) {
+    console.log("element at index ", index, element);
 }
-function fromNumberToString(number, base) {
-    number = Math.abs(number);
-    let res = "";
-    do {
-        let digit = number % base;
-        let sym = getSymbol(digit);
-        res = sym + res;
-        number = Math.trunc(number / base);
-
-    } while(number != 0);
-    return res;
-
+arStr.forEach(println);
+let arCodeAscii = arStr.map(function(symbol, index) {
+    return index % 2 == 0 ? symbol.charCodeAt() : symbol;
+})
+console.log(arStr, arCodeAscii);
+let sumAscii = arStr.reduce(function(res, cur) {
+    return res + cur.charCodeAt();
+}, 0)
+console.log("sum of ascii ", sumAscii)
+console.log(arStr.reduce(function(res, cur) {
+return res + cur
+}, ""));
+function checkTeudatZehut(teudatStrNumber) {
+    //TODO
+    //control sum of for even index digit value, for odd index digit * 2
+    //control sum should be divide on 10 with no remainder
+    //example 123456782 => 1 + 4 +3 + 8 +5 + 3 + 7 + 7 + 2 => true
+    //    123456783 => 1 + 4 +3 + 8 +5 + 3 + 7 + 7 + 3 => false
 }
-function getDigit(symbol) {
-    let codeA = 'a'.charCodeAt();
-    let res = symbol < '9' ? +symbol : symbol.charCodeAt() - codeA + 10;
-    return res;
+function generateRandomTeudatZehut() {
+    //TODO
+    //returns string of 9 symbols matching checkTeudatZehut
+    //make 8 random digits from 0 to 9
+    //9 - th symbol should be with accordance of matching
+    //to get random digit Math.round(Math.random() * 9)
 }
-function fromStringToNumber(string, base) {
- 
-    string = string.toLowerCase();
-    let result = 0;
-    for (let i = 0; i < string.length; i++) {
-        let digit = getDigit(string[i]);
-        result = result * base + digit;
-    }
-    return result;
-}
-
-//getting code from symbol 
-//console.log('abc'.charCodeAt(2))
-
-//getting symbol from code
-//console.log(String.fromCharCode(126))
-
-console.log(fromNumberToString(900550,36))
-console.log(fromStringToNumber("123", 10));
-console.log(fromStringToNumber("10", 16));
-
-
-
-
-
-
-
 
