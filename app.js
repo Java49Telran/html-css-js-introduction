b64a7cf3025a0e4f73e5ce6371f39ec6ebad5d05
Yuragranovsky <yuragranovsky@gmail.com>
2022-11-23 20:16:57 +0200

    adds HW #18 isAnagram solution

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-// function getAddressKey() {
+function isAnagram(str1, str2) {
+    let res = false;
+    if (str1.length == str2.length) {
+        const str1Occurrences = getSymbolOccurrences(str1);
+        res = checkAnagram(str1Occurrences, str2);
+    }
+    return res;
+
+}
+function symbolProcessing(res, cur) {
+    if (res[cur]) {
+        res[cur]++;
+    } else {
+        res[cur] = 1;
+    }
+    return res;
+}
+function getSymbolOccurrences(string) {
+    const stringArray = Array.from(string);
+    return stringArray.reduce(symbolProcessing
+        , {});
+}
+
+function checkAnagram(occurrences, string) {
+    const stringArray = Array.from(string);
    
-//     return "address";
-// }
-// const person = {id: 123, first_name: 'Vasya',
-//  last_name: 'Ivanov', year: 2000, address: {city: 'Lod',
-//   street: 'Sokolov', app: 100}
-// };
-// const name1 = getAddressKey();
-
-// console.log(person[name1])
-// const personArr = [123, 'Vasya', 'Ivanov', 2000,
-//  ['Lod', 'Sokolov', 100]]
-//  console.log(personArr[3])
-//  console.log(person.address);
- function displayOccurrences(strings) {
-    const occurrences = {};
-    strings.forEach(element => {
-        if (occurrences[element]) {
-            occurrences[element]++;
-        } else {
-            occurrences[element] = 1;
+    return stringArray.every(symbol => {
+        let res = false;
+        if (occurrences[symbol]) {
+            res = true;
+            occurrences[symbol]--
         }
-    });
-    //console.log(Object.entries(occurrences));
-    const occurrencesAr = Object.entries(occurrences)
-    occurrencesAr.sort((e1, e2) => e2[1] - e1[1])
-    console.log(occurrencesAr);
+        return res;
 
- }
-//  const x = {};
-//  const string = "abc";
-//  x[string] = 1;
-//  console.log(x);
-//  x[string]++;
-//  console.log(x);
-const strings = ["a","opr","lmn", "abc", "lmn","abc", "lmn", "lmn", "abc", "a"];
-displayOccurrences(strings);
+    })
+
+}
+isAnagram("yellow","oolley")
+
