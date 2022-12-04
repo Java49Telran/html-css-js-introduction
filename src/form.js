21617cbd69c03818206647eadb98ecf5383bf946
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-04 15:09:34 +0200

    adds HW #21 solution

--- src/form.js
+++ src/form.js
@@ -1,4 +1,12 @@
 const inputElements = document.querySelectorAll(".form-class [name]");
+const MIN_SALARY = 1000;
+const MAX_SALARY = 40000;
+const MIN_YEAR = 1950;
+const maxYear = getMaxYear();
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const ERROR_CLASS = "error";
+const dateErrorElement = document.getElementById("date_error");
+const salaryErrorElement = document.getElementById("salary_error");
 function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
@@ -9,11 +17,45 @@ function onSubmit(event) {
         }, {}
     )
     console.log(employee)
+    
 }
 function onChange(event) {
+
     if (event.target.name == "salary") {
-        if(+event.target.value < 1000 || +event.target.value > 40000){
-            event.target.value=''
-        }
+        validateSalary(event.target)
+    } else if (event.target.name == "birthDate") {
+        validateBirthdate(event.target);
     }
+}
+function validateSalary(element) {
+    const value = +element.value;
+    if (value < MIN_SALARY || value > MAX_SALARY) {
+        const message = value < MIN_SALARY ? `salary must be ${MIN_SALARY} or greater`
+            : `salary must be ${MAX_SALARY} or less`;
+        showErrorMessage(element, message, salaryErrorElement);
+    }
+
+}
+function validateBirthdate(element) {
+    const value = +element.value.slice(0, 4);
+    if (value < MIN_YEAR || value > maxYear) {
+        const message = value < MIN_YEAR ? `year must be ${MIN_YEAR} or greater`:
+             `year must be ${maxYear} or less`;
+        showErrorMessage(element, message, dateErrorElement) ;    
+
+    }
+
+}
+function showErrorMessage(element, message, errorElement) {
+    element.classList.add(ERROR_CLASS);
+    errorElement.innerHTML = message;
+    setTimeout(() => {
+        element.classList.remove(ERROR_CLASS);
+        element.value = ''; 
+        errorElement.innerHTML = '';
+    }, TIME_OUT_ERROR_MESSAGE);
+}
+
+function getMaxYear() {
+    return new Date().getFullYear();
 }
